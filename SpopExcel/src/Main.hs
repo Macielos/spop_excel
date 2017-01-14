33b66fb1a0ea19970102bb361d51b9456c999342
Macielos <maciek.ganko@gmail.com>
2017-01-14 23:56:54 +0100

    typ komorki range,, spory refaktor wstawiania komorek i rozszerzania sheeta

--- SpopExcel/src/Main.hs
+++ SpopExcel/src/Main.hs
@@ -5,12 +5,29 @@ import System.IO
 import Data.Sequence as DS
 
 process :: [String] -> Sheet -> Either (IO String) (IO Sheet)
-process parts sheet = case (parts !! 0) of
-    "quit" -> Left $ return "quit"
-    "print" -> Right $ (printSheet sheet)
-    "addRow" -> Right $ return (addRow sheet)
-    "setInt" -> Right $ return (setInt sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int) (read (parts !! 3) :: Int))
-    "setString" -> Right $ return (setString sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int) (parts !! 3))
+process parts sheet =
+    if Prelude.length parts < 1
+    then Left $ return ""
+    {-TODO
+    - walidacja parametrow
+    - przed zawolaniem setFunc sprawdzic czy w range sa tylko komorki number lub empty
+    w sumie przed wstawieniem komorki text lub func tez trzeba by sprawdzac czy nie jest w przedziale funkcji jakiejs innej komorki
+    bo inaczej mogloby sie zawolac liczenie np sredniej ze stringow (ale to mozna i pomijac) albo sredniej z pol gdzie jest funkcja
+    wtedy trzeba by sprawdzac czy nie dojdzie do nieskonczonej rekursji
+    -}
+    else case (parts !! 0) of
+        "quit" -> Left $ return "quit"
+        "help" -> Left $ return printHelp
+        "print" -> Right $ (printSheet sheet)
+        "clear" -> Right $ return (clear sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int))
+        "set" -> Right $ return (set sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int) (parts !! 3))
+        "setFunc" -> Right $ return (setFunc sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int) (parts !! 3) (read (parts !! 4) :: Int) (read (parts !! 5) :: Int) (read (parts !! 6) :: Int) (read (parts !! 7) :: Int))
+        otherwise -> Left $ return "incorrect command, type 'help' for available commands"
+        --TODO newSheet, rename, save, open
+
+printHelp :: String
+printHelp = "[tu bedzie help]"
+
 split :: Eq a => a -> [a] -> [[a]]
 split d [] = []
 split d s = x : split d (Prelude.drop 1 y) where (x,y) = span (/= d) s
@@ -26,20 +43,22 @@ mainLoop sheet = do
               let result = process parts sheet
               case result of
                   Left e -> do
-                            x <- e
-                            if x == "quit"
+                            response <- e
+                            if response == "quit"
                             then putStrLn "quitting..."
+                            else if response == ""
+                            then do
+                                 mainLoop sheet
                             else do
-                                 putStrLn "zjebalo sie bo jest error "
+                                 putStrLn response
                                  mainLoop sheet
                   Right s -> do
                             sheet <- s
-                            putStrLn "sheet wciaz dycha"
                             mainLoop sheet
 
-
-haskellExcel = let sheet = Sheet "testsheet" 0 0 DS.empty
-               in do putStrLn "Witaj w haskellowym arkuszu kalkulacyjnym: "
+haskellExcel = let sheet = newSheet "the sheet has no name"
+               in do putStrLn "Welcome to haskell spreadsheet "
+                     putStrLn "Type 'help' for available commands "
                      mainLoop sheet
 
 main = haskellExcel
--- SpopExcel/src/Sheet.hs
+++ SpopExcel/src/Sheet.hs
@@ -1,4 +1,18 @@
-module Sheet where
+module Sheet (
+newSheet,
+set,
+setFunc,
+clear,
+printSheet,
+Sheet(..)
+{-
+expandSheetIfNecessary,
+getCells,
+getName,
+getWidth,
+getHeight,
+-}
+) where
 
 import Data.Sequence as DS
 
@@ -6,42 +20,115 @@ data Sheet = Sheet {
     name :: String,
     width :: Int,
     height :: Int,
-    cells :: Seq (Seq Cell)
-}
+    cells :: Cells
+} deriving (Show, Eq)
 
-newRow :: Seq Cell
-newRow = DS.fromList([Empty, Empty, Empty])
+type Cells = Seq (Seq Cell)
 
-data Cell = Empty | TextCell {str :: String} | NumberCell {value :: Int} deriving (Show, Eq)
+data Cell =
+    Empty |
+    TextCell {str :: String} |
+    NumberCell {value :: Float} |
+    SumCell {range :: Range} |
+    ProductCell {range :: Range} |
+    MeanCell {range :: Range}
+    deriving (Show, Eq)
 
+data Range = Range {
+    x1 :: Int,
+    y1 :: Int,
+    x2 :: Int,
+    y2 :: Int
+} deriving (Show, Eq)
+
+
+
+{-TODO
+sformatowac to sensownie,
+getCells printowac w formie tabeli,
+dla typow sum, product, mean obliczac je przed wyswietleniem, moze niech implementuja jakis typ z funkcja getValue albo cos
+-}
 printSheet :: Sheet -> IO Sheet
-printSheet sheet = do print (getName sheet)
-                      print (getCells sheet)
+printSheet sheet = do print sheet
                       return sheet
 
-addRow :: Sheet -> Sheet
-addRow sheet = Sheet (getName sheet) (getWidth sheet) (getHeight sheet + 1) ((getCells sheet) |> newRow)
-
-setInt :: Sheet -> Int -> Int -> Int -> Sheet
-setInt sheet row column value = Sheet (getName sheet) (getWidth sheet) (getHeight sheet) (DS.update row (DS.update column (NumberCell value) (index (getCells sheet) row)) (getCells sheet))
-
-setString :: Sheet -> Int -> Int -> String -> Sheet
-setString sheet row column str = Sheet (getName sheet) (getWidth sheet) (getHeight sheet) (DS.update row (DS.update column (TextCell str) (index (getCells sheet) row)) (getCells sheet))
 
 
-{-
-set :: Sheet -> Int -> Int -> Cell -> Sheet
 
-set sheet row column value = Sheet (getName sheet) (getWidth sheet) (getHeight sheet) (Map.insert (row, column) value (getCells sheet))
 
-get :: Sheet -> Int -> Int -> IO ()
+set :: Sheet -> Int -> Int -> String -> Sheet
+set sheet x y value = setCell sheet x y (newCell value)
 
-get sheet row column = do
-    let result = Map.lookup (row, column) (getCells sheet)
-    case result of
-        Nothing -> putStrLn "not found"
-        Just x -> putStrLn "found"
---}
+setFunc :: Sheet -> Int -> Int -> String -> Int -> Int -> Int -> Int -> Sheet
+setFunc sheet x y function x1 y1 x2 y2 = setCell sheet x y (newFuncCell function x1 y1 x2 y2)
+
+clear :: Sheet -> Int -> Int -> Sheet
+clear sheet x y = setCell sheet x y Empty
+
+setCell :: Sheet -> Int -> Int -> Cell -> Sheet
+setCell sheet x y value =
+    Sheet (getName sheet) (max (x+1) (getWidth sheet)) (max (y+1) (getHeight sheet))
+    (setCellInternal (expandSheetIfNecessary (getCells sheet) x y) x y value)
+
+setCellInternal :: Cells -> Int -> Int -> Cell -> Cells
+setCellInternal cells x y value = DS.update y (DS.update x value (index cells y)) cells
+
+
+
+
+{-TODO pewnie mozna to lepiej zrobic-}
+newCell :: String -> Cell
+newCell value =
+    if value == ""
+    then Empty
+    else case reads value :: [(Float, String)] of
+              [(_, "")] -> NumberCell (read value :: Float)
+              _         -> TextCell value
+
+newFuncCell :: String -> Int -> Int -> Int -> Int -> Cell
+newFuncCell function x1 x2 y1 y2 = newFuncCellRange function (Range x1 y1 x2 y2)
+
+newFuncCellRange :: String -> Range -> Cell
+newFuncCellRange function range = case function of
+    "sum" -> SumCell range
+    "product" -> ProductCell range
+    "mean" -> MeanCell range
+    _      -> Empty
+
+newSheet :: String -> Sheet
+newSheet name = Sheet name 1 1 (DS.singleton(newRow 1))
+
+newRow :: Int -> Seq Cell
+newRow length = DS.fromList(map toEmpty [x | x <-[0..(length-1)]])
+
+toEmpty a = Empty
+
+
+
+expandSheetIfNecessary :: Cells -> Int -> Int -> Cells
+expandSheetIfNecessary cells x y = addRowsIfNecessary (addColumnsIfNecessary cells x) y
+
+addRowsIfNecessary :: Cells -> Int -> Cells
+addRowsIfNecessary cells y =
+    if (DS.length cells) < (y + 1)
+    then addRowsIfNecessary (cells |> newRow (DS.length (index cells 0))) y
+    else cells
+
+addColumnsIfNecessary :: Cells -> Int -> Cells
+addColumnsIfNecessary cells x =
+    if (DS.length (index cells 0)) < (x + 1)
+    then expandRow cells 0 (x + 1 - DS.length (index cells 0))
+    else cells
+
+expandRow :: Cells -> Int -> Int -> Cells
+expandRow cells rowNr elementsToAdd =
+    if (rowNr < DS.length cells)
+    then expandRow (DS.update rowNr ((index cells rowNr) >< newRow elementsToAdd) cells) (rowNr+1) elementsToAdd
+    else cells
+
+
+
+
 getCells :: Sheet -> Seq (Seq Cell)
 getCells (Sheet name width height cells) = cells
 
