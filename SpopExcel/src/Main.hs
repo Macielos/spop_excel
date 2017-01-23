cebe109b5db5ee4e9f117263e78f6664292c2040
ypaseka <zhenik.93@gmail.com>
2017-01-23 04:08:33 +0100

    validate functions  is it Int or String

--- SpopExcel/src/Main.hs
+++ SpopExcel/src/Main.hs
@@ -6,11 +6,11 @@ import System.IO
 import Data.Sequence as DS
 import Data.Char
 import Data.Foldable
-import Prelude
+import Prelude as P
 
 process :: [String] -> Sheet -> Either (IO String) (IO Sheet)
 process parts sheet =
-    if (Prelude.length parts < 0)
+    if (P.length parts < 1)
     then Left $ return ""
 
     else case (parts !! 0) of
@@ -18,19 +18,27 @@ process parts sheet =
         "help" -> Left $ return printHelp
         "print" -> Right $ (printSheet sheet)
         "clear" -> do
-                   if ((isInt (parts !! 1)) && (isInt (parts !! 2)) == True)
+                   if (P.length parts < 3)
+                   then Left $ return "A few arguments for function 'clear'"
+                   else if ((isInt (parts !! 1)) && (isInt (parts !! 2)) == True)
                    then Right $ return (clear sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int))
                    else Left $ return "Error - input Integer only"
         "get" -> do
-                 if ((isInt (parts !! 1)) && (isInt (parts !! 2)) == True)
+                 if (P.length parts < 3)
+                 then Left $ return "A few arguments for function 'get'"
+                 else if ((isInt (parts !! 1)) && (isInt (parts !! 2)) == True)
                  then Right $ (printCellDetails sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int))
                  else Left $ return "Error - input Integer only"
         "set" -> do
-                 if ((isInt (parts !! 1)) && (isInt (parts !! 2)) && (Prelude.length parts == 4) == True)
+                 if (P.length parts < 4)
+                 then Left $ return "A few arguments for function 'set'"
+                 else if ((isInt (parts !! 1)) && (isInt (parts !! 2)) == True)
                  then Right $ return (set sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int) (parts !! 3))
                  else Left $ return "Error - input Integer only"
         "setFunc" -> do
-                     if ((isInt (parts !! 1)) && (isInt (parts !! 2)) && (isInt (parts !! 4))&& (isInt (parts !! 5))&& (isInt (parts !! 6))&& (isInt (parts !! 7))== True)
+                     if (P.length parts < 8)
+                     then Left $ return "A few arguments for function 'setFunc'"
+                     else if ((isInt (parts !! 1)) && (isInt (parts !! 2)) && (isInt (parts !! 4))&& (isInt (parts !! 5))&& (isInt (parts !! 6))&& (isInt (parts !! 7))== True)
                      then Right $ return (setFunc sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int) (parts !! 3) (read (parts !! 4) :: Int) (read (parts !! 5) :: Int) (read (parts !! 6) :: Int) (read (parts !! 7) :: Int))
                      else Left $ return "Error - input Integer only"
         otherwise -> Left $ return "incorrect command, type 'help' for available commands"
@@ -42,11 +50,11 @@ isInt [] = True
 isInt (x:xs) = (isDigit x) && (isInt xs)
 
 printHelp :: String
-printHelp = "[tu bedzie help]\ndruga linia\ntrzecia linia"
+printHelp = "Avalible commands:\n 1.set - arguments X Y value\n 2.get - arguments X Y\n 3.clear - arguments X Y\n 4.print - display current sheet\n 5.setFunc - ???????????\n 6.help\n 7.quit"
 
 split :: Eq a => a -> [a] -> [[a]]
 split d [] = []
-split d s = x : split d (Prelude.drop 1 y) where (x,y) = span (/= d) s
+split d s = x : split d (P.drop 1 y) where (x,y) = span (/= d) s
 
 readCommand :: IO String
 readCommand = do
