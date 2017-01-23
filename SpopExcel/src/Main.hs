e3d2ab6d75062fb43dd7c6c5ee210553e2f821f4
ypaseka <zhenik.93@gmail.com>
2017-01-23 03:10:03 +0100

    validate functions  is it Int or String

--- SpopExcel/src/Main.hs
+++ SpopExcel/src/Main.hs
@@ -5,29 +5,34 @@ import FileOperations
 import System.IO
 import Data.Sequence as DS
 import Data.Char
+import Data.Foldable
+import Prelude
 
 process :: [String] -> Sheet -> Either (IO String) (IO Sheet)
 process parts sheet =
-    if Prelude.length parts < 1
+    if (Prelude.length parts < 1)
     then Left $ return ""
-    {-TODO
-    - walidacja parametrow
-    - przed zawolaniem setFunc sprawdzic czy w range sa tylko komorki number lub empty
-    w sumie przed wstawieniem komorki text lub func tez trzeba by sprawdzac czy nie jest w przedziale funkcji jakiejs innej komorki
-    bo inaczej mogloby sie zawolac liczenie np sredniej ze stringow (ale to mozna i pomijac) albo sredniej z pol gdzie jest funkcja
-    wtedy trzeba by sprawdzac czy nie dojdzie do nieskonczonej rekursji-}
 
     else case (parts !! 0) of
         "quit" -> Left $ return "quit"
         "help" -> Left $ return printHelp
         "print" -> Right $ (printSheet sheet)
-        "clear" -> Right $ return (clear sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int))
-        "get" -> Right $ (printCellDetails sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int))
+        "clear" -> do
+                     if ((isInt (parts !! 1)) && (isInt (parts !! 2)) == True)
+                     then Right $ return (clear sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int))
+                     else Left $ return "Error - input Integer only"
+        "get" ->  do
+                    if ((isInt (parts !! 1)) && (isInt (parts !! 2)) == True)
+                    then Right $ (printCellDetails sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int))
+                    else Left $ return "Error - input Integer only"
         "set" -> do
                    if ((isInt (parts !! 1)) && (isInt (parts !! 2)) == True)
                    then Right $ return (set sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int) (parts !! 3))
-                   else Left $ return "Error"
-        "setFunc" -> Right $ return (setFunc sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int) (parts !! 3) (read (parts !! 4) :: Int) (read (parts !! 5) :: Int) (read (parts !! 6) :: Int) (read (parts !! 7) :: Int))
+                   else Left $ return "Error - input Integer only"
+        "setFunc" -> do
+                       if ((isInt (parts !! 1)) && (isInt (parts !! 2)) && (isInt (parts !! 4)&& (isInt (parts !! 5)&& (isInt (parts !! 6)&& (isInt (parts !! 7))== True)
+                       then Right $ return (setFunc sheet (read (parts !! 1) :: Int) (read (parts !! 2) :: Int) (parts !! 3) (read (parts !! 4) :: Int) (read (parts !! 5) :: Int) (read (parts !! 6) :: Int) (read (parts !! 7) :: Int))
+                       else Left $ return "Error - input Integer only"
         otherwise -> Left $ return "incorrect command, type 'help' for available commands"
         --TODO newSheet, rename, save, open
 
